abbdaf085517ce99bf5a41f7624e031e1545fbcd
CabilbiganIPT-102 <cabilbigan.jeheu22@southernchristiancollege.edu.ph>
2024-09-12 09:20:12 -1000

    web app

---
+++ CabilbiganSolution - Copy/CabilbiganCompany/Table/EMPLOYEE.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[EMPLOYEE]
+(
+	[Fname] VARCHAR(15) NOT NULL, 
+    [Minit] CHAR(10) NULL, 
+    [Lname] VARCHAR(15) NOT NULL, 
+    [Ssn] CHAR(9) NOT NULL, 
+    [Bdate] DATE NULL, 
+    [Address] VARCHAR(30) NULL, 
+    [Sex] CHAR(10) NULL, 
+    [Salary] DECIMAL(10, 2) NULL, 
+    [Super_ssn] CHAR(9) NULL, 
+    [Dno] INT NOT NULL 
+    constraint [PK_Employee_Ssn] primary key ([Ssn])
+);
+GO
+
+create nonclustered index[IX,Employee_Name] ON [dbo].[Employee]([Lname] ASC, [Fname] ASC);
---
+++ CabilbiganSolution - Copy/CabilbiganCompany/Table/PROJECT.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PROJECT]
+(
+	[Pname] VARCHAR(15) NOT NULL, 
+    [Pnumber] INT NOT NULL, 
+    [PLocation] VARCHAR(15) NULL, 
+    [Dnum] INT NOT NULL 
+)
+GO
+CREATE NONCLUSTERED INDEX[IX_PROJECT_location] ON [PROJECT] ([Plocation] asc)
