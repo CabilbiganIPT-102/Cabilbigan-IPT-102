9fd021a709590a5ede0e9b37b9b1f55e86b449a3
CabilbiganIPT-102 <cabilbigan.jeheu22@southernchristiancollege.edu.ph>
2024-09-17 00:22:46 -1000

    push

--- CabilbiganSolution - Copy/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ CabilbiganSolution - Copy/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abbdaf085517ce99bf5a41f7624e031e1545fbcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
